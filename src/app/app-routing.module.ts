5c4f58a64001062acceda372e62e78bedd61f243
Shivansh <shivansh.s@thinkbridge.in>
2022-05-12 23:19:45 +0530

    3654 Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CellsComponent } from './cells/cells.component';
+import { CounterComponent } from './counter/counter.component';
+import { CrudComponent } from './crud/crud.component';
+import { FlightBookerComponent } from './flight-booker/flight-booker.component';
+import { TempConverterComponent } from './temp-converter/temp-converter.component';
+import { TimerComponent } from './timer/timer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'counter',component: CounterComponent},
+  {path:'tempConverter',component: TempConverterComponent},
+  {path:'flightBooker',component: FlightBookerComponent},
+  {path:'timer',component: TimerComponent},
+  {path:'crud',component: CrudComponent},
+  {path:'cells',component: CellsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
