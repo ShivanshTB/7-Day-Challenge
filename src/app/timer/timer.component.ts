3589c4396c9148d85ae930c40815d06df257223b
Shivansh <shivansh.s@thinkbridge.in>
2022-05-17 00:48:24 +0530

    3661 timer

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.css']
+  styleUrls: ['./timer.component.css'],
 })
 export class TimerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  sec: number = 0;
+  rangeValue: Number = 30;
 
   ngOnInit(): void {
+    const set = setInterval(() => {
+      this.sec++;
+      if (this.sec == this.rangeValue) {
+        clearInterval(set);
+      }else if(this.sec > this.rangeValue){
+        clearInterval(set);
+      }
+    },1000);
   }
 
+  resetTime(){
+    this.sec = 0;
+    this.ngOnInit();
+  }
+  changeRange(){
+    this.ngOnInit();
+  }
 }
